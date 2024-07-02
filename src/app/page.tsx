dfe527eb99445ffa00d415986b4649104747ff6a
Michael-Builds <michaelkpantramp@gmail.com>
2024-07-02 11:26:48 +0000

    Working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic"
+
 import getCurrentUser from "./actions/getCurrentUser"
 import getListings, { IListingsParams } from "./actions/getListings"
 import ClientOnly from "./components/ClientOnly"
@@ -10,7 +12,6 @@ interface HomeProps {
 }
 
 const Home = async ({ searchParams }: HomeProps) => {
-
   const listings = await getListings(searchParams)
   const currentUser = await getCurrentUser()
 
@@ -26,11 +27,11 @@ const Home = async ({ searchParams }: HomeProps) => {
     <ClientOnly>
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {listings.map((listing, index: any) => {
+          {listings.map((listing) => {
             return (
               <ListingCard
                 currentUser={currentUser}
-                key={index}
+                key={listing.id}
                 data={listing}
               />
             )
